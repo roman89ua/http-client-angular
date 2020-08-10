b10aeec11b974483dd84b9790a8664e2e86f8c83
roman89ua <roman89ua@gmail.com>
2020-08-10 16:09:02 +0300

    headers of requests: 2 types of syntax

--- src/app/todos.service.ts
+++ src/app/todos.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable, throwError} from 'rxjs';
 import {ToDo} from './todo.interface';
 import {catchError, delay} from 'rxjs/operators';
@@ -12,7 +12,11 @@ export class TodosService {
   constructor(private http: HttpClient) { }
 
   addTodo(todo: ToDo): Observable<ToDo>{
-    return this.http.post<ToDo>('https://jsonplaceholder.typicode.com/todos', todo)
+    return this.http.post<ToDo>('https://jsonplaceholder.typicode.com/todos', todo, {
+      headers: new HttpHeaders({
+        MyNewCustomHeader: 'Some value', // first way to add headers
+      })
+    })
       .pipe(catchError(error => {
         console.log(error.message);
         return throwError(error);
@@ -31,7 +35,12 @@ export class TodosService {
   }
 
   remove(id: number): Observable<void> {
-    return this.http.delete<void>(`https://jsonplaceholder.typicode.com/todos/${id}`)
+    const headers = new HttpHeaders({
+      theSecondOneHeader: 'the value of the second header',
+    });
+    return this.http.delete<void>(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+      headers, // the second way to add headers
+    })
       .pipe(catchError(error => {
           console.log(error.message);
           return throwError(error);
