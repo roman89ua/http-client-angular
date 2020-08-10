77f2908554b41872acb3990b7cb6de1352793abb
roman89ua <roman89ua@gmail.com>
2020-08-10 15:14:49 +0300

    second way to handle errors

--- src/app/todos.service.ts
+++ src/app/todos.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {Observable, throwError} from 'rxjs';
 import {ToDo} from './todo.interface';
-import {delay} from 'rxjs/operators';
+import {catchError, delay} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,21 +12,40 @@ export class TodosService {
   constructor(private http: HttpClient) { }
 
   addTodo(todo: ToDo): Observable<ToDo>{
-    return this.http.post<ToDo>('https://jsonplaceholder.typicode.com/todos', todo);
+    return this.http.post<ToDo>('https://jsonplaceholder.typicode.com/todos', todo)
+      .pipe(catchError(error => {
+        console.log(error.message);
+        return throwError(error);
+      })
+    );
   }
 
   fetchTodos(): Observable<ToDo[]> {
     return this.http.get<ToDo[]>('https://jsonplaceholder.typicode.com/todos?_limit=10')
-      .pipe(delay(500));
+      .pipe(delay(500),
+        catchError(error => {
+          console.log(error.message);
+          return throwError(error);
+        })
+      );
   }
 
   remove(id: number): Observable<void> {
-    return this.http.delete<void>(`https://jsonplaceholder.typicode.com/todos/${id}`);
+    return this.http.delete<void>(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .pipe(catchError(error => {
+          console.log(error.message);
+          return throwError(error);
+        })
+      );
   }
 
   complete(id: number): Observable<ToDo>{
     return this.http.put<ToDo>(`https://jsonplaceholder.typicode.com/todos/${id}`, {
       completed: true
-    });
+    }).pipe(catchError(error => {
+          console.log(error.message);
+          return throwError(error);
+        })
+      );
   }
 }
